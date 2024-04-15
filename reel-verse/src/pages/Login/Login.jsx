da7c754dc0133f409dde5ffb7115c2505d6dcf33
Panea Bianca <panea.robertabianca@yahoo.com>
2024-04-15 20:41:40 +0300

    Finalizing Order Imports (#3)
    order imports

--- reel-verse/src/pages/Login/Login.jsx
+++ reel-verse/src/pages/Login/Login.jsx
@@ -1,7 +1,8 @@
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../firebase/firebase";
 
