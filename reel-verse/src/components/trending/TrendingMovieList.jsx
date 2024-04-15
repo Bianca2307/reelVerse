054104935a21e972bf44ce723874258669cb9884
Panea Bianca <panea.robertabianca@yahoo.com>
2024-04-15 20:49:01 +0300

    Finalizing Formatting Code (#4)
    formatting code

--- reel-verse/src/components/trending/TrendingMovieList.jsx
+++ reel-verse/src/components/trending/TrendingMovieList.jsx
@@ -6,13 +6,11 @@ import MovieCard from "../common/MovieCard";
 
 
 export default function TrendingMovieList(props) {
-   
     return (
         <div className="container">
             {props.topRated.map((movie) => (
                 <Link to={`/movies/${movie.id}`} key={movie.id}>
                     <MovieCard
-                       
                         title={movie.title}
                         image={movie.poster_path}
                         date={movie.release_date}
